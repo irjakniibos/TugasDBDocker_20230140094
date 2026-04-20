1673f536999cd0b5549b8179c2ef50c5d45ccbfa
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2026-04-20 11:38:02 +0700

    implement Lombok annotations for boilerplate reduction

--- src/main/java/com/praktikum7db/praktikum7db/model/User.java
+++ src/main/java/com/praktikum7db/praktikum7db/model/User.java
@@ -4,9 +4,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
 @Table(name = "20230140082_users")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     private String id;
